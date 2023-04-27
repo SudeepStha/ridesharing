21dda72c9b45060e8092c5711a47495fabaeb7eb
Tej Bahadur Shrestha <venture.tej@gmail.com>
2023-04-27 13:34:43 +0545

    trip section manage

--- database/migrations/2023_04_21_060825_create_trips_table.php
+++ database/migrations/2023_04_21_060825_create_trips_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('trips', function (Blueprint $table) {
+        Schema::create('trips', function (Blueprint $table) { 
             $table->id();
             $table->foreignIdFor(User::class);
-            $table->foreignIdFor(Driver::class);
+            $table->foreignIdFor(Driver::class)->nullable();
             $table->boolean('is_started')->default(false);
             $table->boolean('is_complete')->default(false);
             $table->json('origin')->nullable();
